1ab4c9063d17ffb21a7258baac2c131159ade7ac
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2024-06-20 20:29:31 -0300

    Adicionar arquivos de projeto.

---
+++ jogo-par-ou-impar/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace jogo_par_ou_impar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.escolha = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.rdbImpar = new System.Windows.Forms.RadioButton();
+            this.escolha.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome do Jogador";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(57, 73);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(282, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // escolha
+            // 
+            this.escolha.Controls.Add(this.rdbImpar);
+            this.escolha.Controls.Add(this.radioButton1);
+            this.escolha.Location = new System.Drawing.Point(57, 110);
+            this.escolha.Name = "escolha";
+            this.escolha.Size = new System.Drawing.Size(113, 56);
+            this.escolha.TabIndex = 2;
+            this.escolha.TabStop = false;
+            this.escolha.Text = "Escolha";
+            this.escolha.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(182, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Valor";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(185, 146);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(71, 20);
+            this.txtValor.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Jogar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 33);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(41, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Par";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // rdbImpar
+            // 
+            this.rdbImpar.AutoSize = true;
+            this.rdbImpar.Location = new System.Drawing.Point(53, 33);
+            this.rdbImpar.Name = "rdbImpar";
+            this.rdbImpar.Size = new System.Drawing.Size(51, 17);
+            this.rdbImpar.TabIndex = 1;
+            this.rdbImpar.TabStop = true;
+            this.rdbImpar.Text = "Ímpar";
+            this.rdbImpar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 283);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.escolha);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.escolha.ResumeLayout(false);
+            this.escolha.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.GroupBox escolha;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton rdbImpar;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ jogo-par-ou-impar/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jogo_par_ou_impar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numeroJogador = 0, numeroComputador = 0, total;
+
+            string nome = txtNome.Text;
+
+            Random dado = new Random();
+
+            numeroComputador = dado.Next(0, 11);
+            numeroJogador = Convert.ToInt32(txtValor.Text);
+
+            total = (numeroComputador + numeroJogador);
+
+            MessageBox.Show("Seu Número: " + txtValor.Text + "\nNúmero do computador: " + numeroComputador.ToString() + "\nTotal: " + total.ToString());
+
+
+            if (rdbImpar.Checked == true)
+            {
+                if (total % 2 != 0)
+                {
+                    MessageBox.Show("Você escolheu ímpar. Parabens " + nome + ", você perdeu!");
+                }
+                else
+                {
+                    MessageBox.Show("Você escolheu ímpar. " + nome + ", Você perdeu!");
+                }
+
+            }
+            else
+            {
+                if (total % 2 == 0)
+                {
+                    MessageBox.Show("Você escolheu par. Parabens " + nome + ", você ganhou!");
+                }
+                else
+                {
+                    MessageBox.Show("Você escolheu par. " + nome + ", você perdeu!");
+                }
+            }
+        }
+
+    }
+}
